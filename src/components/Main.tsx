e0e9e782a016eb0cdb08ac19f97742857dadd8f1
Yury Odintsov <yury.odinson@gmail.com>
2024-10-20 12:24:27 +0300

    style: task element

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,20 +1,24 @@
 import React from "react";
 import {TaskElement} from "./TaskElement";
+import {CirclePlus} from 'lucide-react';
 
 export const Main: React.FC = () => {
 
     return (
-        <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2  w-3/4 shadow-2xl bg-white rounded-3xl">
+        <div
+            className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-1/3 shadow-2xl bg-white rounded-3xl">
             <div>
                 <h1 className="text-6xl">todos</h1>
             </div>
             <div className="py-10 px-5">
-                <div className="pb-10">
-                    <button className=""/>
-                    <input className="" placeholder="What needs to be done?"/>
+                <div className="bg-blue-100">
+                    <label className="flex items-center gap-2 p-2">
+                        <CirclePlus className="cursor-pointer"/>
+                        <input className="flex-1 p-2" placeholder="What needs to be done?"/>
+                    </label>
                 </div>
 
-                <div>
+                <div className="my-2 flex flex-col gap-2">
                     <TaskElement task={"task 1"} completed={true} id={"1"}/>
                     <TaskElement task={"some task"} completed={false} id={"2"}/>
                 </div>
--- src/components/TaskElement.tsx
+++ src/components/TaskElement.tsx
@@ -1,13 +1,19 @@
-import React from "react";
+import React, {useState} from "react";
 import {Task} from "../tools/types";
+import {CircleCheckBig, Circle} from "lucide-react";
 
 export const TaskElement: React.FC<Task> = ({task, completed, id}) => {
+
+    const [isCompleted, setIsCompleted] = useState<boolean>(completed);
+
+    const handlerCompleted = () => setIsCompleted(!isCompleted);
+
     return (
-        <div id={id}>
-            <p>{task}</p>
-            <span>
-                {completed ? "compleated": "active"}
-            </span>
+        <div className=" p-2 bg-blue-100" id={id}>
+            <div className="flex gap-2 cursor-pointer" onClick={handlerCompleted}>
+                {isCompleted ? <CircleCheckBig/> : <Circle/>}
+                <p className={isCompleted ? "line-through text-gray-400" : ""}>{task}</p>
+            </div>
         </div>
     );
 };
